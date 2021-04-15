a761b89fb53663d7c57c65dac802a17faf24a7f6
Prasenjit Dey <prasenjit1367@gmail.com>
2021-04-15 13:57:45 +0530

    new code

--- codepipeline_Fargate/vars.tf
+++ codepipeline_Fargate/vars.tf
@@ -24,4 +24,51 @@ variable "ECS_AMIS" {
   }
 }
 
+variable "ENVIRONMENT" {
+    type    = string
+    default = "development"
+}
+
 # Full List: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
+
+variable "LEVELUP_VPC_CIDR_BLOC" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "LEVELUP_VPC_PUBLIC_SUBNET1_CIDR_BLOCK" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.101.0/24"
+}
+
+variable "LEVELUP_VPC_PUBLIC_SUBNET2_CIDR_BLOCK" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.102.0/24"
+}
+
+variable "LEVELUP_VPC_PUBLIC_SUBNET3_CIDR_BLOCK" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.103.0/24"
+}
+
+variable "LEVELUP_VPC_PRIVATE_SUBNET1_CIDR_BLOCK" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "LEVELUP_VPC_PRIVATE_SUBNET2_CIDR_BLOCK" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "LEVELUP_VPC_PRIVATE_SUBNET3_CIDR_BLOCK" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.3.0/24"
+}
